8b4f19af1e048222be6408707caab44749d9e228
aneeqakbar <aneeqakbqr@gmail.com>
2021-10-14 00:40:48 +0500

    done

--- core/cdn/conf.py
+++ core/cdn/conf.py
@@ -1,9 +1,7 @@
 import os
 
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_ACCESS_KEY_ID = "KUYV5H7JWOMNYF7I46XG"
-AWS_SECRET_ACCESS_KEY = "4W0FwRSCXjoJnmXQl+oP7uwtNiiE0oh+eIfzBFJ63XQ"
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = "manyaka"
 AWS_S3_ENDPOINT_URL = "https://nyc3.digitaloceanspaces.com/"
 AWS_LOCATION = "https://manyaka.nyc3.digitaloceanspaces.com/"
